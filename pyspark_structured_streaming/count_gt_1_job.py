4fec02dcfdd7fbc3a30c18cf60fcd7c074ee1a59
Vitor Plentz <vitor.plentz@elixirai.com.br>
2022-07-11 15:50:05 -0300

    Removing printSchema from count_gt_1_job.py

--- pyspark_structured_streaming/count_gt_1_job.py
+++ pyspark_structured_streaming/count_gt_1_job.py
@@ -10,16 +10,15 @@ df = domain_conn.read_from_kafka('wikipedia.parsed')
 
 df = df.filter(col('value.meta.domain') == 'commons.wikimedia.org')
 
-df.printSchema()
-
 df = df.groupBy(window(col('timestamp'), "5 minutes"), 
                 col('value.user').alias('user'),
                 col('value.meta.uri')).count().withColumnRenamed('count', 'COUNT')
 
 df =  df.withColumn('key', to_json(struct(col('user'), col('uri').alias('URI'))))
+
 df =  df.withColumn('value', struct(col('COUNT')))
+
 df = df.select(col('key'), col('value'))
-df.printSchema()
 
 domain_conn.load_to_kafka(df, 'WIKIPEDIA_COUNT_GT_1', 'update')
 
